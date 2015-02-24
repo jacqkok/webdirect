fdd0bf3c24e1722956acdf87df280ed82ff399c6
fkwakkenbos <f.kwakkenbos@flooris.nl>
2015-02-24 17:30:30 +0100

    Responsiveness geoptimaliseerd. Onnodige elementen verborgen. Header tekst toegevoegd. Footer tekst toegeveogd. Styling buttons.

--- FM13-server-webdirect/page-home/hsleiden_homepage.js
+++ FM13-server-webdirect/page-home/hsleiden_homepage.js
@@ -11,7 +11,6 @@ function setPageStyle() {
         x = w.innerWidth || e.clientWidth || g.clientWidth,
         y = w.innerHeight|| e.clientHeight|| g.clientHeight;
 
-
     var protocol    = l.protocol+'//';
     var hostname    = l.hostname;
     var url_current = l.href;
@@ -34,8 +33,8 @@ function setPageStyle() {
     /**
      * Start timeout
      */
-    timeout_key = setTimeout(setFrameStyle, 500);
-
+    //timeout_key = setTimeout(setFrameStyle, 500);
+    timeout_key = setTimeout(setFooterText, 500);
 
     /**
      * Example:
@@ -43,21 +42,51 @@ function setPageStyle() {
      */
 }
 
+function setFooterText() {
+    var frame_elem          = document.getElementById(iframe_id);
+    var elem_footer_html    = document.getElementById('footer_html');
+
+    var buttons_elem        = frame_elem.contentDocument.getElementsByClassName('v-slot-primary');
+    var item_buttons        = buttons_elem.item(0);
+
+    if ( typeof(item_buttons) === "undefined" || null === item_buttons ) {
+        return timeout_key = setTimeout(setFooterText, 500);
+    }
+
+    var items_buttons_plac1 = item_buttons.parentElement;
+    var items_buttons_plac2 = items_buttons_plac1.parentElement;
+    var elem_footer_new     = items_buttons_plac2.appendChild(elem_footer_html);
+    elem_footer_new.style.display = 'block';
+
+    var header_label        = frame_elem.contentDocument.getElementsByClassName('v-label-login_header');
+    var item_header_label   = header_label.item(0);
+    item_header_label.innerText = 'Log in met je gebruikersnaam (= eerste deel van je e-mailadres) en bijbehorend wachtwoord.';
+}
+
 function setFrameStyle() {
     var frame_elem          = document.getElementById(iframe_id);
     var login_elem_shadow   = frame_elem.contentDocument.getElementsByClassName('v-shadow-window');
     var login_elem_wrap     = frame_elem.contentDocument.getElementsByClassName('v-window');
 
-    var maximize_elem       = frame_elem.contentDocument.getElementsByClassName('v-window-maximizebox');
+    var buttons_elem        = frame_elem.contentDocument.getElementsByClassName('v-slot-primary');
+
+    // v-slot-primary
 
     var item_login_shadow   = login_elem_shadow.item(0);
     var item_login_wrap     = login_elem_wrap.item(0);
+    var item_buttons        = buttons_elem.item(0);
+
+    var is_mobile = isMobile.any();
 
     if ( typeof(item_login_shadow) === "undefined" || null === item_login_shadow ) {
         return timeout_key = setTimeout(setFrameStyle, 500);
     }
 
     if ( item_login_shadow.hasAttributes('style') ) {
+        if (1 > timeout_extra_delay) {
+            return timeout_key = setTimeout(setFrameStyle, 1500);
+        }
+
         var pos_left_raw    = item_login_shadow.style.left;
         var pos_left_str    = pos_left_raw.replace("px", "");
         var pos_left        = parseFloat(pos_left_str);
@@ -73,11 +102,18 @@ function setFrameStyle() {
         var pos_left_dif    = (width_new - width_original);
         var pos_left_new    = pos_left - (pos_left_dif / 2);
 
+        /*if (is_mobile) {
+            var pos_left_new    = pos_left - pos_left_dif;
+        }*/
+
         /**
          * Set left positions 2 elements
          */
-        item_login_shadow.style.left    = pos_left_new + "px";
+        item_login_shadow.style.left = pos_left_new + "px";
         item_login_wrap.style.left   = pos_left_new + "px";
+
+        item_login_shadow.style.top = "50px";
+        item_login_wrap.style.top = "50px";
 
         var w = window,
             d = document,
@@ -87,10 +123,28 @@ function setFrameStyle() {
             x = w.innerWidth || e.clientWidth || g.clientWidth,
             y = w.innerHeight|| e.clientHeight|| g.clientHeight;
 
-        var frame_body = frame_elem.contentDocument.getElementsByClassName('v-generated-body');
-        var item_frame_body = frame_body.item(0);
-
+        var frame_body        = frame_elem.contentDocument.getElementsByClassName('v-generated-body');
+        var item_frame_body   = frame_body.item(0);
         item_frame_body.style.width = x + "px";
+
+        var header_label      = frame_elem.contentDocument.getElementsByClassName('v-label-login_header');
+        var item_header_label = header_label.item(0);
+        item_header_label.innerText = 'Log in met je gebruikersnaam (= eerste deel van je e-mailadres) en bijbehorend wachtwoord.';
+
+
+        var elem_footer_html = document.getElementById('footer_html');
+        var footer_html = elem_footer_html.innerHTML;
+
+        var items_buttons_plac1 = item_buttons.parentElement;
+        var items_buttons_plac2 = items_buttons_plac1.parentElement;
+        var elem_footer_new = items_buttons_plac2.appendChild(elem_footer_html);
+        elem_footer_new.style.display = 'block';
+
+
+        /*if (is_mobile && 1 > mobile_set_style_counter && 800 < x) {
+            mobile_set_style_counter++;
+            return timeout_key = setTimeout(setFrameStyle, 2000);
+        }*/
 
     } else {
         /**
@@ -99,3 +153,26 @@ function setFrameStyle() {
         return timeout_key = setTimeout(setFrameStyle, 500);
     }
 }
+
+
+
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
